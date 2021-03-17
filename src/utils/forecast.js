3f59f3e30c323598ee66e69e09d4b608f5224f8d
Tianli Chen <tchen@tianlis-mbp.propark.com>
2021-03-17 11:59:12 -0400

    Add more weather descriptions

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(body.error.info, undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike 
-            + ' degrees out. The humidity is ' + body.current.humidity + '%. Wind diretion is ' + body.current.wind_dir)
+            + ' degrees out. The humidity is ' + body.current.humidity + '%. Wind direction is ' + body.current.wind_dir + '.')
         }
     })
 }
